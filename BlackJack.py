deb8f911f6a16680d1f20e90ee2647837eae65f9
yui666a <haiso666@gmail.com>
2019-11-13 13:45:51 +0900

    first commit

---
+++ BlackJack.py
@@ -0,0 +1,292 @@
+""" 
+BlackJackGame
+情報工学特論
+
+参考文献
+https://gist.github.com/shiracamus/8c74cf6b97863c4ab56f607889cdfc1f
+
+
+
+Copyright:
+    Fumi (https://fumisite.net/)
+
+License:
+    GNU General Public License version 3.
+    (https://www.gnu.org/licenses/gpl-3.0.html)
+
+"""
+"""''''''''''''''''''''''''''''''''ルール'''''''''''''''''''''''''''''''''''''''''''''''
+
+
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''"""
+
+#-*- coding:utf-8 -*-
+
+import random
+ 
+#-------------------------------------------------------
+deck = []               #デック
+deck_count = 0          #デックのインデント
+deck_change_count = 1   #デック交換の回数
+
+card_count = 0          #カウンティング数
+
+money = 10000           #所持金
+bet = 100               #ベット数(固定)
+
+SUITS = ('Spade', 'Heart', 'Diamond', 'Club')
+RANKS = ('A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K')
+VALUES = ((1,11), 2, 3, 4, 5, 6, 7, 8, 9, 10)
+COUNTS = ( -1, 0, 1)    #ハイローシステム  ランニングカウントは1デックあたりの合計が0なので初期設定IRCは0でよい、今のところ正規化なし
+
+#------------------------------------------------------
+def get_value(rank, sum):   
+    if(rank == 'J' or rank == 'Q' or rank == 'K'):
+        return VALUES[9]
+
+    elif(rank == 'A' and sum + 11 <= 21):
+        return VALUES[0][1]
+
+    elif(rank == 'A' and sum + 11 > 21):
+        return VALUES[0][0]
+
+    else:
+        return VALUES[int(rank)-1]
+
+def get_count(rank):    #ハイローシステム
+    if(rank == '10' or rank == 'J' or rank == 'Q' or rank == 'K' or rank == 'A'):
+        return COUNTS[0]
+
+    elif(rank == '7' or rank == '8' or rank == '9'):
+        return COUNTS[1]
+
+    else:
+        return COUNTS[2]
+
+def Deck():
+    deck = []
+    for k in range(6):
+        for i in range(4):
+            for j in range(13):
+                deck.append((SUITS[i], RANKS[j]))
+
+    random.shuffle(deck)
+    return deck
+
+def p_result(p, d):
+    global money
+    global bet
+    get_money = 0       #報酬用に変数生成
+    if(p.get_rst() == 4):
+        print('{0}の負け！'.format(p.name))
+    elif(d.get_rst() == 4 and p.cards_total() <= 21):
+        print('{0}の勝利！'.format(p.name))
+        get_money = 2 * bet
+    else:
+        if(p.cards_total() > d.cards_total()):
+            print('{0}の勝利！'.format(p.name))
+            p.set_rst(1)
+            get_money = 2 * bet
+        elif(p.cards_total() == d.cards_total()):
+            print('{0}は引き分け！'.format(p.name))
+            p.set_rst(2)
+            get_money = bet
+        else:
+            print('{0}の負け！'.format(p.name))
+            p.set_rst(3)
+
+    money += get_money
+    print(money)
+
+def o_result(o, d):
+    if(o.get_rst() == 4):
+        print('{0}の負け！'.format(o.name))
+    elif(d.get_rst() == 4 and o.cards_total() <= 21):
+        print('{0}の勝利！'.format(o.name))
+    else:
+        if(o.cards_total() > d.cards_total()):
+            print('{0}の勝利！'.format(o.name))
+            o.set_rst(1)
+        elif(o.cards_total() == d.cards_total()):
+            print('{0}は引き分け！'.format(o.name))
+            o.set_rst(2)
+        else:
+            print('{0}の負け！'.format(o.name))
+            o.set_rst(3)
+
+#-------------------------------------------------------
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.Cards()
+        self.flag = True    #ヒットできるか否か、True：できる。つまりFalseはスタンド、バーストのいずれかが起こった時
+        self.rst = 0        #(0:試合中,1:勝ち,2:引き分け,3:負け, 4:バースト)
+
+    def Cards(self):
+        self.cards = []
+
+    def cards_hit(self):
+        global deck_count
+        global deck
+        global card_count
+        self.cards.append(deck[deck_count])
+        card_count += get_count(deck[deck_count][1])
+        print('name:{0}, 引いたカード:{1}'.format(self.name, deck[deck_count]))
+        value_sum = self.cards_total()
+        print('合計:{0}'.format(value_sum))
+        if(value_sum > 21):
+            self.flag = False
+            print('バースト')
+            self.rst = 4
+        print('カードカウンティング合計:{0}'.format(card_count))
+        deck_count += 1 
+
+    def cards_total(self):
+        sum = 0
+        A_count = 0;
+        for a in self.cards:
+            if(a[1] == 'A'):
+                A_count += 1
+                continue
+            sum += get_value(a[1], sum)
+
+        #print(sum)
+        #print(A_count)
+        for i in range(A_count):
+            sum += get_value('A', sum)
+        return sum
+
+    def get_flag(self):
+        return self.flag
+
+    def set_flag(self, flag):
+        self.flag = flag
+
+    def get_rst(self):
+        return self.rst
+
+    def set_rst(self, num):
+        self.rst = num
+
+class Others(Player):
+    def Action(self):
+        global deck_count
+        global deck
+        if(self.cards_total() >= 17):
+            print('合計:{0}'.format(self.cards_total()))
+            self.set_flag(False)
+        while(self.flag):
+            self.cards_hit()
+            if(self.cards_total() >= 17):
+                self.set_flag(False)
+
+class Dealer(Others):
+    FIRSTCOUNT = 0  #最初に配られるカードのカウンティイングの値を保存するやつ
+    FIRSTCARD = ('','')      #最初に配られたカード
+    def ini_cards_hit(self):
+        global deck_count
+        global deck
+        global card_count
+        self.cards.append(deck[deck_count])
+        self.FIRSTCARD = deck[deck_count]
+        self.FIRSTCOUNT = get_count(deck[deck_count][1]) 
+        deck_count += 1 
+
+    def sec_cards_hit(self):
+        global deck_count
+        global deck
+        global card_count
+        self.cards.append(deck[deck_count])
+        card_count += get_count(deck[deck_count][1])
+        print('name:{0}, 引いたカード:{1}'.format(self.name, deck[deck_count]))
+        print('カードカウンティング合計:{0}'.format(card_count))
+        deck_count += 1
+
+    def Action(self):
+        global deck_count
+        global deck
+        print('合計:{0}'.format(self.cards_total()))
+        if(self.cards_total() >= 17):
+            self.set_flag(False)
+        while(self.flag):
+            self.cards_hit()
+            if(self.cards_total() >= 17):
+                self.set_flag(False)
+
+    def get_FIRSTCOUNT(self):
+        return self.FIRSTCOUNT
+
+    def get_FISTCARD(self):
+        return self.FIRSTCARD
+
+#----------------------------------------------------------
+def main():
+    global deck
+    global deck_count
+    global deck_change_count
+    global money
+    global bet 
+    global card_count
+    deck = Deck()
+
+    while(1):
+        print("-------------------------------------------------------------------")
+
+        player = Player('Player')
+        other1 = Others('Other1')
+        other2 = Others('Other2')
+        dealer = Dealer('Dealer')
+
+        if(deck_count > 218):
+            deck_count = 0
+            deck = Deck()
+            print('デック交換')
+            deck_change_count+=1
+
+        print("Game Start")
+        money -= bet
+        player.cards_hit()
+        other1.cards_hit()
+        other2.cards_hit()
+        dealer.ini_cards_hit()
+        player.cards_hit()
+        other1.cards_hit()
+        other2.cards_hit()
+        dealer.sec_cards_hit()
+
+        print("Player Turn")
+        #プレイヤーの行動記述-------------------------------------------------------------------------------
+
+        while(player.get_flag()):
+            print('[call:y, stand:n]:')
+            num = input('>>')
+            if(num == 'y'):
+                player.cards_hit()
+            else:
+                player.set_flag = False
+                break
+
+        #------------------------------------------------------------------------------------------------
+
+        print("Other1 Turn")
+        other1.Action()
+
+        print("Other2 Turn")
+        other2.Action()
+
+        print("Dealer Turn")
+        card_count += dealer.get_FIRSTCOUNT()
+        print('ディーラーの最初のカード：{0}'.format(dealer.get_FISTCARD()))
+        dealer.Action()
+
+        p_result(player, dealer)
+        o_result(other1, dealer)
+        o_result(other2, dealer)
+
+        print('現在出た枚数：{0}'.format(deck_count))
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ GameSystem.py
@@ -0,0 +1,477 @@
+"""
+Verflucht!のゲームシステム
+
+int型の入力のみで動作します．
+
+クリーチャーや武器の選択もリストのインデックスで選択してください．
+
+また，複数の武器を洗濯する時は，インデックスの値の間をスペースで区切ってください
+"""
+#-*- coding:utf-8 -*-
+import random
+
+
+# classes here
+class Card :
+    """
+    カードの情報を保持するクラス.
+
+    Attributes
+    ----------
+    num : int
+        カードの強さ(1~40)
+    isCreature : bool
+        カードが「武器」か「クリーチャー」か判定する
+        true : creature
+        false: weapon
+    """
+    def __init__(self, num, isCreature):
+        self.num : int = num
+        self.isCreature : bool = isCreature
+
+    def __lt__(self, other):
+        """
+        今は小さい順にソートしている．
+        """
+        # self < other
+        return self.num < other.num
+
+class Q:
+    """
+    学習に用いる情報をまとめたクラス.
+
+    S（状態集合の定義）：
+    状態  Life=1  CRT 倒せる   
+    S0   T       4   T  
+    S1   F       4   T  
+    S2   T       5   T  
+    S3   F       5   T  
+    S4   T       6   T  
+    S5   F       6   T  
+    S6   T       4   F  
+    S7   F       4   F  
+    S8   T       5   F  
+    S9   F       5   F  
+    S10  T       6   F  
+    S11  F       6   F  
+    S12                  山札がなくなった
+
+    Attributes
+    ----------
+    state : int
+        状態
+
+    """
+    MAX_STATE : int
+    state : int
+
+    def __init__(self):
+        self.MAX_STATE : int = 12
+        self.state : int = 0
+
+
+# global variable
+startingLife = 3
+MAISU : int = 80
+life : int = startingLife
+# deck =  [Card] * MAISU
+deck = []
+fieldCreature = []
+handWeapon = []
+aCard : Card = Card(-1, True)
+state = Q()
+
+# def here
+def getState():
+    """
+    現在の状態を返す
+
+    Returns
+    -------
+    state : int
+    """
+    return state.state
+
+def getMaxState():
+    """
+    現在の状態を返す
+
+    Returns
+    -------
+    state : int
+    """
+    return state.MAX_STATE
+
+def getStateIsGoal():
+    return state.state == state.MAX_STATE
+
+def init():
+    """
+    カードを初期化し，山札をランダムな順番に並べる
+    また，fieldCreatureやhandWeaponも初期化し，
+    lifeを初期値に戻す
+    """
+    global life, deck, fieldCreature, handWeapon
+    deck.clear()
+    for i in range(MAISU):
+        if(i % 2 == 0):
+            deck.append(Card(1 + int(i/2), True))
+        else:
+            deck.append(Card(1 + int(i/2), False))
+    random.shuffle(deck)
+    fieldCreature.clear()
+    handWeapon.clear()
+    life = startingLife
+
+def draw():
+    """
+    カードを一枚引く
+
+    Parameters
+    ----------
+
+    Returns
+    -------
+    drawnCard : Card
+        引いたカード
+    """
+    global fieldCreature, handWeapon, deck
+    drawnCard = deck[0]
+    deck = deck[1:]
+    if(drawnCard.isCreature == True):
+        fieldCreature.append(drawnCard)
+        fieldCreature.sort()
+        print("{0}\t{1}".format(drawnCard.num, "Creature"))
+    else:
+        handWeapon.append(drawnCard)
+        handWeapon.sort()
+        print("{0}\t{1}".format(drawnCard.num, "Weapon"))
+    return drawnCard
+
+def creaturesBack(creatures : [int]):
+    """
+    バトルし，倒せなかったクリーチャーを山札に戻す
+
+    Parameters
+    ----------
+    creatures : Card(list)
+
+    Returns
+    -------
+
+    Raises
+    ------
+    引数がない場合
+    リストで返されてない場合
+
+    Notes
+    -----
+    連番により連結したクリーチャーに対応するため，パラメータはリストでよろしく!
+    """
+    global fieldCreature, deck
+    clist : Card= []
+    for i in range(creatures.__len__()):
+        clist.append(fieldCreature[creatures[i]])
+    fieldCreature = [i for j, i in enumerate(fieldCreature) if j not in creatures]
+    deck.extend(clist)
+    random.shuffle(deck)
+
+def slay(creatures : [int], weapons : [int]):
+    """
+    場のクリーチャーを退治する
+    退治した場合，武器と場のクリーチャーは削除する.
+    できなかった場合は，何もせずにFalseを返す.
+
+    Parameters
+    ----------
+    creatures : int(list) <-- index
+    weapons   : int(list) <-- index
+    creatures : int(list) <-- 数値(強さ)
+    weapons   : int(list) <-- 数値(強さ)
+
+    Returns
+    -------
+    isSlain : bool
+        退治できたかどうかを示す．
+        True  : 退治できた．
+        False : 退治できなかった.(もしこっちだったらエラー!!)
+
+    Raises
+    ------
+    引数に過不足がある場合
+    リストで返されてない場合
+
+    Notes
+    -----
+    パラメータはリストでよろしく!
+    """
+
+    global fieldCreature, handWeapon
+    sumCreatures : int = 0
+    sumWeapons : int = 0
+
+    try:
+        # TODO : カードがあるかチェックしたい
+        # 武器とクリーチャー，それぞれの合計値を算出する
+        for i in range(creatures.__len__()):
+            sumCreatures += fieldCreature[creatures[i]].num
+        for i in range(weapons.__len__()):
+            sumWeapons += handWeapon[weapons[i]].num
+
+        # 武器の合計値がクリーチャー合計値より低かった場合(失敗)
+        if(sumWeapons < sumCreatures):
+            print("x クリーチャーを退治できませんでした")
+            return False
+
+        # 武器の合計値がクリーチャー合計値より高かった場合(成功)
+        print("○ クリーチャーを退治できました")
+        fieldCreature = [i for j, i in enumerate(fieldCreature) if j not in creatures]
+        handWeapon = [i for j, i in enumerate(handWeapon) if j not in weapons]
+        return True
+    except IndexError:
+        print("範囲内のインデックスで選択してください")
+        print("handWeapon : ", end="")
+        printHandWeapon()
+        print("fieldCreature : ", end="")
+        printFieldCreature()
+        return False
+
+def subLife():
+    global life
+    print("life: {0} -> {1}".format(life, life - 1))
+    life -= 1;
+
+def printDeck():
+    """
+    山札の順番と枚数を出力する(テスト用　使用厳禁)
+    """
+    global deck
+    for i in deck:
+        if(i.isCreature == True):
+            print("{0}\t{1}".format(i.num, "Creature"))
+        else:
+            print("{0}\t{1}".format(i.num, "Weapon"))
+    print("山札残り枚数: {0}".format(deck.__len__()))
+
+def printACard():
+    """
+    カード情報を出力する
+    """
+    global aCard
+    try:
+        print("{0}\t{1}".format(aCard.num, aCard.isCreature))
+    except UnboundLocalError:
+        print("まだカードをひいていません")
+
+def printFieldCreature():
+    """
+    場のクリーチャーを表示
+    """
+    for i in fieldCreature:
+        print("{0} ".format(i.num), end=' ')
+    print()
+
+def printHandWeapon():
+    """
+    手札の武器を表示
+    """
+    for i in handWeapon:
+        print("{0} ".format(i.num), end=' ')
+    print()
+
+def printState():
+    """
+    手札，場のクリーチャー，ライフの数，山札残り枚数を表示
+    """
+    print("handWeapon : ", end="")
+    printHandWeapon()
+    print("fieldCreature : ", end="")
+    printFieldCreature()
+    print("life: {0}".format(life))
+    print("山札残り枚数: {0}".format(deck.__len__()))
+
+def fieldCheck():
+    if(deck.__len__() == 0):
+        print("山札の枚数が0になりました．おめでとうございます!!")
+        clear()
+        return False
+    #連番になった時の動作
+    l = isConsecutive()
+    if(not l == []):
+        print("場のクリーチャーが連番になりました．強制バトルを開始します")
+        forcedBattleConsecutive(l)
+    if(fieldCreature.__len__() >= 6):
+        print("場のクリーチャーが6体になりました．強制バトルを開始します")
+        forcedBattle6()
+    if(life == 0):
+        print("\nGAME OVER")
+        gameOver()
+        return False
+    return True
+
+def clear():
+    """
+    クリア時のスコア計算を行う
+    """
+    global fieldCreature
+    for i in range(life):
+        fieldCreature = fieldCreature[1:]
+    sum = 0;
+    for i in fieldCreature:
+        sum += i.num
+    print("スコア: {0}".format(820 - sum))
+
+def gameOver():
+    global fieldCreature, deck
+    sum = 0;
+    for i in fieldCreature:
+        sum += i.num
+    for i in deck:
+        if(i.isCreature == True):
+            sum += i.num
+    print("スコア: {0}".format(820 - sum - 100))
+
+def forcedBattle6():
+    global life, fieldCreature
+    flag = False
+    while(not flag):
+        # c = []
+        # c.append(fieldCreature[-1])
+        print("\nバトルするクリーチャー: {0}".format(fieldCreature[-1].num))
+        print("手持ちの武器: ", end="")
+        printHandWeapon()
+        if(handWeapon.__len__() == 0):
+            print("手持ちの武器がないため，ライフで受けます.")
+            subLife()
+            creaturesBack([fieldCreature.__len__() - 1])
+            return True
+        else:
+            print("複数の武器を使用する場合は，スペースで区切って入力してくだい．\nライフで受ける場合は，'-1'と入力してくだい")
+            # print("handWeapon index: ", end="")
+            w = list(map(int, input("handWeapon index: ").split()))
+            if(w == [-1]):
+                subLife()
+                creaturesBack([fieldCreature.__len__() - 1])
+                return True
+            else:
+                flag = slay([fieldCreature.__len__() - 1], w)
+
+def isConsecutive():
+    global fieldCreature
+    consecutiveCreatures = []
+    for i in range(fieldCreature.__len__()):
+        try:
+            if(fieldCreature[i - 1].num == fieldCreature[i].num - 1):
+                consecutiveCreatures.append(i - 1)
+                consecutiveCreatures.append(i)
+            if(fieldCreature[i + 1].num == fieldCreature[i].num + 1):
+                consecutiveCreatures.append(i)
+                consecutiveCreatures.append(i + 1)
+        except IndexError:
+            pass
+            # print("IndexError")
+    return list(set(consecutiveCreatures)) #重複を削除
+
+def forcedBattleConsecutive(creatures):
+    global life, fieldCreature
+    flag = False
+
+    s = "\nバトルするクリーチャー: "
+    sum = 0
+    for i in creatures:
+        sum += fieldCreature[i].num
+        s += str(fieldCreature[i].num) + ' '
+    s += '(合計 ' + str(sum) + ')'
+
+    clist : int = []
+    for i in creatures:
+        clist.append(i)
+    while(not flag):
+        print(s)
+
+        print("手持ちの武器: ", end="")
+        printHandWeapon()
+        if(handWeapon.__len__() == 0):
+            print("手持ちの武器がないため，ライフで受けます.")
+            subLife()
+            creaturesBack(clist)
+            return True
+        else:
+            print("複数の武器を使用する場合は，スペースで区切って入力してくだい．\nライフで受ける場合は，'-1'と入力してくだい")
+            # print("handWeapon index: ", end="")
+            w = list(map(int, input("handWeapon index: ").split()))
+            if(w == [-1]):
+                subLife()
+                creaturesBack(clist)
+                return True
+            else:
+                flag = slay(creatures, w)
+                if(not flag):
+                    print("倒せませんでした")
+
+def help():
+    print(" 0 : help \t\t(コマンドリストを表示)")
+    print(" 1 : draw \t\t(カードを一枚引く)")
+    print(" 2 : slay \t\t(クリーチャーを倒す)")
+    print(" 3 : printState \t(現在の状態を出力する)")
+    print(" 4 : fieldCheck \t(使用禁止 デバッグ用)")
+    print(" 5 : printACard \t(使用禁止 デバッグ用)")
+    print(" 6 : printDeck \t\t(使用禁止 デバッグ用)")
+    print("-1 : exit \t\t(ゲームを終了する)")
+
+def chooseCommand(n):
+    """
+    入力を受け付け，コマンドを実行する
+    '-1'と入力された時のみ False を返す.
+    """
+
+    # print("\nCommand:", end=' ')
+    # try:
+    #     n = int(input("\nCommand:　"))
+    # except ValueError:
+    #     print("<数字を入力してください>")
+    #     return True
+
+    if(n == 0):
+        help()
+    elif(n == 1):
+        c = draw()
+    elif(n == 2):
+        printFieldCreature()
+        # print("fieldCreature index: ", end="")
+        c = list(map(int, input("fieldCreature index: ").split()))
+        if(c.__len__() > 1):
+            print("一度に退治できるクリーチャーは１体のみです．")
+            return True
+        # print("handWeapon index: ", end="")
+        printHandWeapon()
+        print("複数の武器を使用する場合は，スペースで区切って入力してくだい．")
+        w = list(map(int, input("handWeapon index: ").split()))
+        slay(c, w)
+    elif(n == 3):
+        printState()
+    elif(n == 4):
+        fieldCheck()
+    elif(n == 5):
+        printACard()
+    elif(n == 6):
+        printDeck()
+    elif(n == -1):
+        return False
+    else:
+        print("コマンドが間違っています")
+    return True
+
+def main():
+    # help()
+    # print("\nAre you ready?  Let's get started!!")
+    init()
+    # flag = True
+    # while(flag):
+    #     flag = chooseCommand()
+    #     if(not flag):
+    #         print("終了します")
+    #         break
+    #     flag = fieldCheck()
+
+main()
